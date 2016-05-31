c5c1786d7e05a9826d5256532d0595941ca156d8
wxy <developerwxy@163.com>
2016-05-31 17:11:16 +0800

    系统配置相关

---
+++ config.js
@@ -0,0 +1,25 @@
+//数据库名称
+const DATABASENAME = "NodeSample";
+/**
+ * config 配置
+ */
+var config = {
+    //mysql 连接字符串配置
+    mysql:{
+        host: "localhost",
+        port: "3306",
+        user: "root",
+        password: "123456",
+        database: DATABASENAME,
+        queryFormat: function (query, values) {
+            if(!values) return query;
+            return query.replace(/\:(\w+)/g, function (txt,key) {
+                if(values.hasOwnProperty(key)){
+                    return this.escape(values[key]);
+                }
+                return txt;
+            }.bind(this));
+        }
+    }
+};
+module.exports = config;
