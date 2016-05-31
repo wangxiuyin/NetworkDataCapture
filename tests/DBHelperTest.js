cea271b95e4ee9be7f8f2c7f09c1b5ced2e73c04
wxy <developerwxy@163.com>
2016-05-31 17:32:51 +0800

    数据库测试类

---
+++ tests/DBHelperTest.js
@@ -0,0 +1,10 @@
+var db = require("../util/DBHelper");
+
+/**
+ * 测试数据库是否连接上
+ */
+// var sqlStr = "select * from NetworkData_WC where `id`= :id";
+// var params = {"id":"df121bee5a214220b3fabf15a0668e25"};
+// db.executeSql(sqlStr, params, function (rows) {
+//     console.log(rows);
+// });
