cfe5dc3acacd67e0199f3b461dcc5c79fe5a4866
wxy <developerwxy@163.com>
2016-06-12 17:32:20 +0800

    新增

---
+++ tests/FileHelperTest.js
@@ -0,0 +1,3 @@
+var fs = require('../util/FileHelper.js');
+
+fs.readFileAsync('FileTest.txt');
---
+++ tests/httpHelperTest.js
@@ -0,0 +1,3 @@
+var url = require('url');
+var str = 'https://www.wacai.com/setting/book_listAll.action';
+console.log(url.parse(str));
