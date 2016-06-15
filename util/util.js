ea38f4b2b837a1bef4c47bcc29cd669a35c5e530
wxy <developerwxy@163.com>
2016-06-15 17:39:10 +0800

    工具类

---
+++ util/util.js
@@ -0,0 +1,16 @@
+/**
+ * 一些帮助方法
+ */
+var validator = require("./validator.js");
+
+
+// console.log(validator.coreString.trim(" dddd "));
+console.log(define.amd);
+
+
+// exports.extend = function () {
+//     var target = arguments[0] || {};
+//     var lenght = arguments.length;
+//     var i = 1;
+
+// }
