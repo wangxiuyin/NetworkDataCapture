6d251b7b07aeffb6ca6acf90348353ea15ae4e6d
wxy <developerwxy@163.com>
2016-06-17 17:32:41 +0800

    目前在测试

--- util/util.js
+++ util/util.js
@@ -1,11 +1,27 @@
 /**
  * 一些帮助方法
  */
-var validator = require("./validator.js");
+// var validator = require("./validator.js");
+// var tool = require("./tools.js");
 
+var util = require("./coreUtil");
+var u = new util();
+console.log(u.isNumeric("n"));
+
+
+var coreString = require("./coreString");
+
+var coreStr = new coreString.CoreString();
+// var str = "2341234_dafadfa_222_dafda_ads";
+// console.log(coreStr.replaceAll(str, "_", "~"));
+// var str = "abcd";
+// var s = coreStr.reverse(str);
+// console.log(str+"--"+s);
+
+console.log(coreStr.isNullOrWriteSpace(""));
 
 // console.log(validator.coreString.trim(" dddd "));
-console.log(define.amd);
+//console.log((new validator.CoreUtil()).isArray(" dd "));
 
 
 // exports.extend = function () {
@@ -14,3 +30,7 @@ console.log(define.amd);
 //     var i = 1;
 
 // }
+
+
+var r = "1999-12-12 12:22:33".match(/^(\d{1,4})(-|\/)(\d{1,2})\2(\d{1,2}) (\d{1,2}):(\d{1,2}):(\d{1,2})$/);
+console.log(r);
